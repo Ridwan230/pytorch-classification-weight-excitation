37fa35441a18886e1d57acf9f94fcd2011dc53b7
Ridwan230 <191.ridwan@gmail.com>
2023-01-13 21:02:41 +0600

    Back to normal AlexNet

--- models/cifar/alexnet.py
+++ models/cifar/alexnet.py
@@ -13,17 +13,17 @@ class AlexNet(nn.Module):
     def __init__(self, num_classes=10):
         super(AlexNet, self).__init__()
         self.features = nn.Sequential(
-            L.Conv2d(3, 64, kernel_size=11, stride=4, padding=5, bias=False),
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=5, bias=False),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            L.Conv2d(64, 192, kernel_size=5, padding=2, bias=False),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2, bias=False),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            L.Conv2d(192, 384, kernel_size=3, padding=1, bias=False),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1, bias=False),
             nn.ReLU(inplace=True),
-            L.Conv2d(384, 256, kernel_size=3, padding=1, bias=False),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1, bias=False),
             nn.ReLU(inplace=True),
-            L.Conv2d(256, 256, kernel_size=3, padding=1, bias=False),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1, bias=False),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
