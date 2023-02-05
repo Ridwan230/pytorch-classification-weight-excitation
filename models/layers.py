d811fb7554b8b6b6f0c769467af9c5869f521134
Ridwan230 <191.ridwan@gmail.com>
2023-02-05 15:39:12 +0600

    MWE stacked inside LWE

--- models/layers.py
+++ models/layers.py
@@ -10,11 +10,7 @@ class Conv2d_Custom(nn.Conv2d):
                  padding=0, dilation=1, groups=1, bias=True):
         super(Conv2d_Custom, self).__init__(in_channels, out_channels, kernel_size, stride,
                                             padding, dilation, groups, bias)
-
         self.count = 0
-        planes = in_channels
-        self.fc1 = nn.Conv2d(planes//groups, max(planes//16, 1), kernel_size=1)
-        self.fc2 = nn.Conv2d(max(planes//16, 1), planes//groups, kernel_size=1)
         self.eps = 1e-5
 
     def forward(self, x):
@@ -22,19 +18,20 @@ class Conv2d_Custom(nn.Conv2d):
         weight = self.weight
 
         # Following 4 lines are implementation of weight standardization. Optional. Seems to work well, except for depthwise convolution. See paper for more details.
-        weight_avg = weight.mean(dim=1, keepdim=True).mean(dim=2,
-                                                           keepdim=True).mean(dim=3, keepdim=True)
-        weight = weight - weight_avg
+        weight_mean = weight.mean(dim=1, keepdim=True).mean(dim=2,
+                                                            keepdim=True).mean(dim=3, keepdim=True)
+        weight = weight - weight_mean
         std = weight.view(weight.size(
             0), -1).std(dim=1).view(-1, 1, 1, 1) + self.eps
         weight = weight / std.expand_as(weight)
-
-        # LWE
-        wght = F.avg_pool2d(weight, weight.size(2))
-        wght = F.relu(self.fc1(wght))
-        wght = F.sigmoid(self.fc2(wght))
-        weight = weight * wght
-
+        # MWE
+        max_val = (1.0 + 0.1) * \
+            torch.max(torch.max(weight), -1 * torch.min(weight))
+        if self.count:
+            weight = max_val * 0.5 * \
+                torch.log((1+weight/max_val) / (1-weight/max_val))
+        else:
+            self.count = 1
         return F.conv2d(x, weight, self.bias, self.stride,
                         self.padding, self.dilation, self.groups)
 
